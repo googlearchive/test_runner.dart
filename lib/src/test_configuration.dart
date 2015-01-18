44be54d35618e49c7182c76aa401469a234a2470
Kevin Moore <kevmoo@google.com>
2015-01-17 16:47:04 -0800

    Removed un-implemented annotations
    See https://github.com/google/test_runner.dart/issues/20
    and https://github.com/google/test_runner.dart/issues/19
    
    For related bugs

--- lib/src/test_configuration.dart
+++ lib/src/test_configuration.dart
@@ -65,20 +65,6 @@ class VmTest extends Test {
   const VmTest({this.disableTransformers: false});
 }
 
-/// Annotate a test main function if the test can't be run in parallel to other.
-/// For example if it uses database read/write conflicting with other tests.
-// TODO need to implement this behavior
-class NotParallelizable {
-  const NotParallelizable();
-}
-
-/// Annotate a test main function if the test is supposed to fail. For example
-/// in the case of Test-driven development.
-// TODO need to implement this behavior
-class ShouldFail {
-  const ShouldFail();
-}
-
 /// Describes a test and its configuration like:
 ///  - Path of the test file
 ///  - How should the test be ran (in browser, command line)
