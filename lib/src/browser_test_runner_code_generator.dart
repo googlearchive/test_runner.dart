719074f4072e8da6747fb80df75eb2e0be3f8c5b
Nicolas Garnier <nivco.las@gmail.com>
2015-01-19 16:14:45 +0100

    Adding missing Default HTML file template.

--- lib/src/browser_test_runner_code_generator.dart
+++ lib/src/browser_test_runner_code_generator.dart
@@ -25,7 +25,7 @@ class BrowserTestRunnerCodeGenerator extends TestRunnerCodeGenerator {
       if (testHtmlFilePath == null || testHtmlFilePath == "") {
         // If the test does not have an associated test file we'll call the test
         // file inside of a default HTML file.
-        return _BROWSER_TEST_DART_FILE_TEMPLATE;
+        return _BROWSER_TEST_HTML_FILE_TEMPLATE;
       } else {
         // Custom HTML test files.
         return new File(testHtmlFilePath).readAsString();
@@ -103,5 +103,29 @@ void main() {
   useHtmlConfiguration();
   test.main();
 }
+''';
+
+/// Template of a Default HTML file for Browser unittest files that will
+/// start the tests in the Dart file written instead of the `{{test_file_name}}`
+/// placeholder.
+const String _BROWSER_TEST_HTML_FILE_TEMPLATE = '''
+<!-- Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+for details. All rights reserved. Use of this source code is governed by a
+BSD-style license that can be found in the LICENSE file. -->
+
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>Default Web Test HTML file</title>
+    <meta charset="utf-8" />
+    <meta name="description" content="Runs a Web test" />
+  </head>
+  <body>
+    <!-- Scripts -->
+    <script type="application/dart" src="/{{test_file_name}}"></script>
+    <script type="text/javascript" src="/packages/unittest/test_controller.js"></script>
+  </body>
+</html>
 ''';
 
